ab2e4770475941759adb474167bd47065097d7de
EilonV <73593619+EilonV@users.noreply.github.com>
2023-08-21 17:09:39 +0300

    day1

---
+++ src/components/day1.jsx
@@ -0,0 +1,18 @@
+export const Day1 = () => {
+    return <div className="frame">
+        <div className="center">
+            <div className="day1">
+                <div className="content">
+                    <div className="hundie">
+                        <div className="one-body" />
+                        <div className="one-head" />
+                        <div className="zero" />
+                        <div className="zero last" />
+                    </div>
+                    <p className="large-text">DAYS</p>
+                    <p className="small-text">CSS CHALLENGE</p>
+                </div>
+            </div>
+        </div>
+    </div>
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,7 @@
+import { Day1 } from "../components/day1"
+
+export const Home = () => {
+    return <section>
+        <Day1 />
+    </section>
+}
